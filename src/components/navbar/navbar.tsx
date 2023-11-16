bd99ab630717eccb2159268372f152240ec6bed4
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-11-16 19:59:56 -0300

    criado components navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,19 @@
+const NavBar=()=>{
+return(
+
+    <div className="bg-lime-600 p-5 shadow-lg flex justify-between">
+        <h1>WattLink</h1>
+        <ul className="flex gap-5">
+            <li>home</li>
+            <li> produtos </li>
+            <li> login </li>
+            
+        </ul>
+
+
+    </div>
+)
+
+}
+
+export {NavBar}
