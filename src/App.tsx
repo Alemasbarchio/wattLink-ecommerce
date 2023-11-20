39346dc7cc70f3dda726c0d7f7f9d9046d2c3cbe
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-11-20 15:36:36 -0300

    atualização landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { LogoContents } from './components/contentsLogo';
+import { ProdList } from './components/listcontainer/listproduct';
 import { NavBar } from './components/navbar/navbar';
 
+
 function App() {
+
+
   return (
     <div className="App">
       <NavBar/>
-  
+      <LogoContents/>
+      <ProdList greeting='Produtos do carrinho'/>
+     
+      
     </div>
   );
 }
---
+++ src/components/contentsLogo/index.tsx
@@ -0,0 +1,16 @@
+
+
+const LogoContents=()=>{
+return(
+
+    <div className="bg-green-900 h-60 flex items-center justify-center flex-col">
+        <h1 className="text-white text-6xl" > WattLink </h1>
+        <p className="text-white text-2xl">Soluções em Energia</p>
+      
+    </div>
+    
+)
+
+}
+
+export{LogoContents}
