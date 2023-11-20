c9a88769e567713cd1869d17b8d8c93bf394632f
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-11-20 15:27:44 -0300

    Criado components CartWidget

---
+++ src/components/cartwidget/cartwidget.tsx
@@ -0,0 +1,13 @@
+//import iconCar from './img/icon_carrinho.svg';
+const CarWid=()=>{
+    return(
+    
+        <div className="cursor-pointer  ">
+            <img className="w-8"  src='./img/icon_carrinho.svg' alt="icone-carrinho"/>
+        </div>
+            
+    )
+    
+    }
+    
+    export {CarWid}
