ffa6433f83190ebcd4c71a6214db154805edf6a8
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-11-20 15:34:23 -0300

    criado componente cartWidget

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,17 +1,24 @@
+import { CarWid } from "../cartwidget/cartwidget"
+
+//import logo from './img/logo-eletrica.png';
 const NavBar=()=>{
 return(
 
-    <div className="bg-lime-600 p-5 shadow-lg flex justify-between">
-        <h1>WattLink</h1>
-        <ul className="flex gap-5 cursor-pointer">
+    <div className="bg-gray-300 p-5 shadow-lg flex justify-evenly">
+       
+       <div>
+        <img className="w-10"  src='./img/logo-eletrica.png' alt="logo"/>
+       
+        </div>
+        <ul className="flex gap-5 cursor-pointer justify-center items-center">
+      
             <li>home</li>
             <li> produtos </li>
             <li> login </li>
-            
         </ul>
-
-
+               <CarWid/>    
     </div>
+    
 )
 
 }
