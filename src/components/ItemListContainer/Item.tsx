f853f658b3bcc8123a1ef96e265493dfa488f817
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-11-26 22:42:35 -0300

    Elaborado componente Item

---
+++ src/components/ItemListContainer/Item.tsx
@@ -0,0 +1,33 @@
+import { CounterItem} from "../Counter/itemcount";
+
+interface Products{
+    id?:number;
+    name:string;
+    price:number;
+    imgProduct:string;
+    qtdStock:number;
+  }
+
+
+const Item=({name,id,price,imgProduct,qtdStock}:Products)=>{
+   const AddToCart = () => {
+    
+  };
+
+    return(
+        
+          <ul>
+            <li className="bg-slate-200 gap-50 p-2">
+            <img className="w-60" src={imgProduct} alt="" />
+            <h1>{id}</h1>
+            <h2 className="text-green-800 font-bold">{name}</h2>
+            <h3>{price}</h3>
+            <CounterItem stock={qtdStock} initial={1} onAdd={AddToCart} />
+            </li>
+          </ul>   
+      
+
+    )
+}
+
+export {Item}
