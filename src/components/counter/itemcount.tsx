8caff1d36135c1fa789bd4ae9937595807340309
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-11-26 22:41:44 -0300

    função subtrair a qtd selecionada do estoque

--- src/components/counter/itemcount.tsx
+++ src/components/counter/itemcount.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 interface ItemCountProps {
   stock: number;
@@ -7,20 +7,35 @@ interface ItemCountProps {
 }
 
 const CounterItem = ({ stock, initial, onAdd }: ItemCountProps) => {
-  const [value, setValue] = useState(initial);
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const [value, setValue] = useState(initial);
+    const [stockQtd, setStockQtd] = useState<number>(stock);
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const inputValue = parseInt(event.target.value);
     setValue(inputValue);
 
   }
+  useEffect(() => {
+    console.log("No estoque tem " + stockQtd);
+  }, [stockQtd])
+
+  const addcart = () => {
+    if (value <= stockQtd && stockQtd !== 0) {
+      console.log(`Adicionar ${value}  itens ao carrinho`);
+      setStockQtd((prevStok => prevStok - value))
+      onAdd(value);
+    }
+    else {
+      console.log("estoque indisponível")
+    }
+  }
 
   return (
 
-    <div className="p-4 flex flex-col items-center justify-center gap-10" >
-      <input type="number" min="1" value={value} onChange={handleInputChange}
-        className="w-32 no-spinner border p-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+    <div className="p-2 flex flex-col items-center justify-center gap-5" >
+      <input type="number" min="1" max={stockQtd} value={value} onChange={handleInputChange}
+        className="w-24 no-spinner border p-2 rounded-md focus:outline-none focus:border-green-600"
       />
-      <button onClick={()=>onAdd(value)} className=" bg-green-600 border p-2 rounded-lg focus:border-blue-300 hover:opacity-75"> Adicionar ao Carrinho</button>
+      <button onClick={addcart} className=" bg-green-600 border p-2 rounded-lg focus:border-blue-300 hover:opacity-75"> Adicionar ao Carrinho</button>
 
     </div>
 
