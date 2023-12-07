f2fa0390cc18bd0d40527a3bea237f78b4d7bf0a
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-12-07 16:27:35 -0300

    Alterado script para abrir produto com base no Params{name}

--- src/components/ItemDetailContainer/ItemDetailContainer.tsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.tsx
@@ -1,67 +1,68 @@
 
 import { useState,useEffect } from "react";
 import productDB from '../../assets/img/disjuntor_bipolar.png';
-import { ItemDetail } from "./itemDetail";
+import productDM from '../../assets/img/disjuntor_monopolar.png';
+import productDt from '../../assets/img/disjuntor_tripolar.png';
+import productLL from '../../assets/img/lampada-led.png';
 import { LogoContents } from "../ContentsLogo";
+import { useParams } from 'react-router-dom';
+import { ItemDetail } from "./itemDetail";
 
 
 
- interface ItemListProps {
- 
-    id: number;
-    name: string;
-    price: number;
-    imgProduct: string;
-    qtdStock: number;
-  
 
+interface ItemListProps {
+  id: number;
+  name: string;
+  price: number;
+  imgProduct: string;
+  qtdStock: number;
 }
 
 const ItemDetailContainer = () => {
-  const [products, setProducts] = useState<ItemListProps[]>([]);
-  
-    const getItem = (): Promise<ItemListProps[]> => {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve([
-            {id:1, name: 'Disjuntor Bipolar 20a',price: 35.55,imgProduct:`${productDB}`,qtdStock:5},
-        
-            
-          ]);
-        }, 500);
-        
-      });
-    
-    };
-    useEffect(() => {
-      const onMount = async () => {
-        try {
-          const productsDataResult = await getItem();
-          setProducts(productsDataResult);
-        } catch (error) {
-          console.error('Ops, ocorreu um erro:', error);
-        
-        }
-      };
-  
-      onMount();
-    }, []); 
+  const { name } = useParams<{ name: string }>();
+    const [product, setProduct] = useState<ItemListProps | null>(null);
 
-  
-  return (
-    <div >
-       <LogoContents/>
-      {products.map((item, index) => (
-        <ItemDetail key={index} {...item} />
-   
-      ))}
+  const getProducts = (): Promise<ItemListProps[]> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([
+          { id: 1, name: 'Disjuntor Bipolar 20a', price: 35.55, imgProduct: `${productDB}`, qtdStock: 5 },
+          { id: 2, name: 'Disjuntor Monopolar 10a', price: 9.85, imgProduct: `${productDM}`, qtdStock: 6 },
+          { id: 3, name: 'Disjuntor Tripolar 60a', price: 65.99, imgProduct: `${productDt}`, qtdStock: 15 },
+          { id: 4, name: 'Lampada Led 9w', price: 12.55, imgProduct: `${productLL}`, qtdStock: 2 },
+          { id: 5, name: 'Lampada Led 15w', price: 15.60, imgProduct: `${productLL}`, qtdStock: 2 },
+        ]);
+      }, 1500);
+    });
+  };
+
+  const getItem = async (): Promise<void> => {
+    try {
       
+      const productsDataResult = await getProducts(); 
+      const productByName = productsDataResult.find((produto) => produto.name === (name));
 
-   
-         
+      if (productByName) {
+        setProduct(productByName);
+      } else {
+        console.error('Produto não encontrado');
+      }
+    } catch (error) {
+      console.error('Ops, ocorreu um erro:', error);
+    }
+  };
+
+  useEffect(() => {
+    getItem();
+  }, [name]);
+
+  return (
+    <div>
+      <LogoContents />
+      {product ? <ItemDetail {...product} /> : <p>Carregando...</p>}
     </div>
   );
-   
 };
 
-export { ItemDetailContainer};
+export default ItemDetailContainer;
