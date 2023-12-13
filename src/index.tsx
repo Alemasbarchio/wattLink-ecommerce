d2a671269410c7ae02e547acd2ee106577b455b5
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-12-12 21:35:35 -0300

    atualização para uso do context

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Routers } from './routers';
+import { CartProductProvider } from './context/cartContext';
+
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Routers/>
+  <CartProductProvider>
+     <Routers/>
+   </CartProductProvider>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
