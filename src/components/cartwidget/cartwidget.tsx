171e17aaa88c1070543c13e997ff6600b5a11a6f
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-12-12 21:27:53 -0300

    Adicionado rota do IconCar  e contagem de itens

--- src/components/cartwidget/cartwidget.tsx
+++ src/components/cartwidget/cartwidget.tsx
@@ -1,9 +1,24 @@
+import { Link } from 'react-router-dom';
 import iconCar from '../../assets/img/icon_carrinho.svg';
+import { useProductCart } from '../../context/cartContext';
 const CarWid=()=>{
+
+const {cartProduts}=useProductCart();
+
+
     return(
     
-        <div className="cursor-pointer  ">
-            <img className="w-8"  src={iconCar} alt="icone-carrinho"/>
+        <div className="cursor-pointer flex justify-end pr-16">
+           
+        <Link to={`/carrinho/`}>
+        <div 
+        className="bg-cover bg-center w-10 h-10"
+        style={{ backgroundImage: `url(${iconCar})` }}
+            >
+       <p className='text-white flex justify-center items-center font-semibold border rounded-full bg-red-600 w-4 h-4'>{cartProduts.length}</p>
+        </div>
+
+          </Link>
         </div>
             
     )
