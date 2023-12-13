f92da8ec3bee90ca03a4bab691ef5f58f1ba407d
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-12-12 21:33:24 -0300

    atualização

--- src/components/ItemDetailContainer/itemDetail.tsx
+++ src/components/ItemDetailContainer/itemDetail.tsx
@@ -2,44 +2,42 @@ import { CounterItem } from "../Counter/itemcount";
 
 
 
-interface ItemDetailProps{
-  
-  name:string;
-  price:number;
-  imgProduct:string;
-  qtdStock:number;
-  
+interface ItemDetailProps {
+  name: string;
+  price: number;
+  imgProduct: string;
+  qtdStock: number;
 
-         
+
+
 }
 
-const ItemDetail=({name,price,imgProduct,qtdStock}:ItemDetailProps)=>{
-
- 
+const ItemDetail = ({ name, price, imgProduct, qtdStock }: ItemDetailProps) => {
 
   const AddToCart = () => {
-    
+
   };
-  
-     return(
 
-        <div className="pt-5">
-         
-         <ul className="flex justify-center items-center text-center  " >
-       
-          <li className="border-green-300 border p-2  w-2/12 flex flex-col">
-          <h2 className="text-green-800 font-bold text-center text-4xl" >{name}</h2>
-          <img src={imgProduct}/> 
+  return (
+
+    <div className="pt-5">
+
+      <ul className="flex justify-center  text-center gap-10 pr-10  " >
+
+        <li className="border-green-300 border p-2  w-2/12 flex flex-col">
+          <h2 className="text-green-800 font-bold text-center text-3xl" >{name}</h2>
+          <img src={imgProduct} />
           <p className="text-2xl"> {price}</p>
-          <CounterItem stock={qtdStock} initial={1} onAdd={AddToCart}/>
-       
-          </li> 
-          </ul>           
-    
-        </div>
-    )
-     
-    }
+          <CounterItem stock={qtdStock} initial={1} onAdd={AddToCart} />
+
+        </li>
+
+      </ul>
+
+    </div>
+  )
+
+}
 
 
-export {ItemDetail};
+export { ItemDetail };
