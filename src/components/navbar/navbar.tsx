017b8c6537db01a0b51438d1cbf3855e0001e242
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-12-12 21:30:59 -0300

    Adiconado rotas

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,26 +1,29 @@
 import { CarWid } from "../CartWidget/CartWidget"
 
 import logo from '../../assets/img/logo-eletrica.png';
-const NavBar=()=>{
-return(
+import { Link } from "react-router-dom";
+const NavBar = () => {
+    return (
 
-    <div className="bg-gray-300 p-5 shadow-lg flex justify-evenly">
-       
-       <div>
-        <img className="w-10"  src={logo} alt="logo"/>
-       
+        <div className="bg-gray-300 p-5 shadow-lg flex justify-evenly h-12 items-center">
+
+            <div>
+                <img className="w-8" src={logo} alt="logo" />
+
+            </div>
+            <ul className="flex gap-5 cursor-pointer justify-center items-center">
+                <Link to={`/`}>
+                    <li>home</li>
+                </Link>
+
+                <li> produtos </li>
+                <li> login </li>
+            </ul>
+            <CarWid />
         </div>
-        <ul className="flex gap-5 cursor-pointer justify-center items-center">
-      
-            <li>home</li>
-            <li> produtos </li>
-            <li> login </li>
-        </ul>
-               <CarWid/>    
-    </div>
-    
-)
+
+    )
 
 }
 
-export {NavBar}
+export { NavBar }
