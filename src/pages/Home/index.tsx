fb39d28abf4aa7ac89d0b642022f6822a1a54659
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2023-12-21 22:45:40 -0300

    footer adicionado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,22 @@
 
 import { LogoContents } from '../../components/ContentsLogo';
+import { Footer } from '../../components/Footer/footer';
 import { ProdList } from '../../components/ItemListContainer/ListProduct';
 import { NavBar } from '../../components/navbar/navbar';
+import { useProductCart } from '../../context/cartContext';
 
 function Home() {
-  
+  const{products}=useProductCart();
   return (
 
-    <div className='text-center'>
+    <div className='text-center flex flex-col gap-5'>
       <NavBar />
       <LogoContents />
       <ProdList greeting='Produtos do carrinho' />
+      {products.length!=0?<Footer/>:null}
+      
+     
+      
           
     </div>
   );
