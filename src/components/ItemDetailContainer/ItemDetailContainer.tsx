9c505217d9f14cc4b3b5ffa30478403034573584
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2024-01-05 19:33:36 -0300

    atualizado formtação

--- src/components/ItemDetailContainer/ItemDetailContainer.tsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.tsx
@@ -1,7 +1,7 @@
 
 import { useState, useEffect } from "react";
 import { LogoContents } from "../ContentsLogo";
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { ItemDetail } from "./itemDetail";
 import { useProductCart } from '../../context/cartContext';
 import { NavBar } from "../navbar/navbar";
@@ -20,28 +20,17 @@ interface ItemListProps {
 
 const ItemDetailContainer = () => {
   const { name } = useParams<{ name: string }>();
-  const { products, cartProduts } = useProductCart();
+  const { products} = useProductCart();
  
-
   const [detailProduct, setDetailProduct] = useState<ItemListProps | null>(null);
   
-  const logImg = cartProduts.find((produto) => produto.name === name);
   
   const getProducts = (): Promise<ItemListProps[]> => {
     return new Promise((resolve) => {
       setTimeout(() => {
 
-        // procurar informação no carrinho para trazer valores atualizados para o itemDetail
-        const searchName = cartProduts.some((produto) => produto.name === (name))
-        if (cartProduts.length != 0 && searchName) {
-          resolve(cartProduts);
-          
-        }
-
-        if (cartProduts.length >= 0 || searchName) {
-          resolve(products);
-        }
-     
+         resolve(products);
+    
       }, 1000);
     });
   };
